b016f14153a392d193b8a5d1f18a8420492fd1aa
justi <justi@192.168.254.3>
2018-07-08 23:09:26 -0400

    update1

--- WhileLoops/WhileLoop.cpp
+++ WhileLoops/WhileLoop.cpp
@@ -2,14 +2,37 @@
  * WhileLoop.cpp
  *
  *  Created on: Jul 8, 2018
- *      Author: justi
+ *      Author: Nathaniel Parizi
  */
 
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
 
+	int i = 0;
+
+	//while loop C++
+	while (i <= 5) {
+		cout << "Hello" << flush;
+		i = i + 1;
+		cout << i << endl;
+	}
+
+	int q = 0;
+
+	//do while loop C++
+	do {
+		cout << "q: " << q << endl;
+		q++;
+	} while (q <= 5);
+
+	i = 50;
+	if (i == 50) {
+		cout << "i is 50" << endl;
+	}
+
+	cout << "Program terminated" << endl;
 
 
 
